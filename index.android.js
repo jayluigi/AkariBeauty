ae8987d2e9b6fc6c5089e130cd3d873ed11f9991
jayluigi <jayluigi@gmail.com>
2016-10-04 20:06:19 +0100

    Added BlinkingText and LotsOfStyles

--- index.android.js
+++ index.android.js
@@ -13,6 +13,55 @@ import {
   Image
 } from 'react-native';
 
+
+
+/*********************************************************************************
+ * Blinking text App
+ ********************************************************************************/
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // Toggle state in constructor
+    setInterval( () => {
+      this.setState({showText: !this.state.showText});
+    }, 600);
+    
+  }
+
+  render()
+  {
+    let display = this.state.showText ? this.props.Text : '';
+    return(
+      <Text style={styles.video}>{display}</Text>
+    );
+  }
+}
+
+class BlinkApp extends Component
+{
+  render()
+  {
+    return(
+      <View>
+        <Blink Text = "Test Title 01" />
+        <Blink Text = "Test Title 02" />
+        <Blink Text = "Test Title 03" />
+        <Blink Text = "Test Title 04" />
+      </View>
+    )
+  }
+}
+/********************************************************************************
+ * End of App
+ *******************************************************************************/
+
+
+
+/********************************************************************************
+ * Youtube Channel Playlist App
+ *******************************************************************************/
 class Thumbnail extends Component {
   render() {
       let pic = {
@@ -49,21 +98,60 @@ class Playlist extends Component{
       <View>
         <Video />
         <Video />
-        <Video />
-        <Video />
-        <Video />               
+        <Video />          
+      </View>
+    );
+  }
+}
+/********************************************************************************
+ * End of App
+ *******************************************************************************/
+
+
+
+/********************************************************************************
+ * Lots of styles App
+ *******************************************************************************/
+
+class LotsOfStyles extends Component
+{
+  render()
+  {
+    return(
+      <View>
+        <Text style = {styles.red}>just red</Text>
+        <Text style = {styles.bigblue}>big fat blue</Text>
+        <Text style = {[styles.bigblue, styles.red]}>big and fat and blue, but RED</Text>
+        <Text style = {[styles.red, styles.bigblue]}>red, then big blue applied</Text>
       </View>
     );
   }
 }
 
+/********************************************************************************
+ * End of App
+ *******************************************************************************/
+
 const styles = StyleSheet.create({
-  video: {
+  video: 
+  {
     margin: 10,
     alignItems: 'center',
   },
-  title: {
-  }
+  title: 
+  {
+    margin: 10,
+  },
+  bigblue:
+  {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 28,
+  },
+  red:
+  {
+    color: 'red',
+  },
 });
 
-AppRegistry.registerComponent('Akari', () => Playlist); // launcher component
+AppRegistry.registerComponent('Akari', () => LotsOfStyles); // launcher component
